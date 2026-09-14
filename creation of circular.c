8d85000a0d4b1aa5a1056abb1e29e6aa00e339c2
mayankchauhan1211 <mayank.chauhan.cse.2025@miet.ac.in>
2026-09-14 14:29:08 +0530

    Update creation of circular.c

--- creation of circular.c
+++ creation of circular.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Function to print the circular linked list
 void printList(struct Node* head) {
     if (head == NULL) {
         printf("The list is empty.\n");
@@ -17,7 +15,6 @@ void printList(struct Node* head) {
     struct Node* temp = head;
     printf("Circular Linked List: ");
     
-    // Use a do-while loop because it's circular
     do {
         printf("%d -> ", temp->data);
         temp = temp->next;
@@ -26,7 +23,6 @@ void printList(struct Node* head) {
     printf("(back to head: %d)\n", head->data);
 }
 
-// Function to insert a node at the end of the list
 struct Node* insertEnd(struct Node* head, int data) {
     // Create a new node
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
@@ -36,19 +32,16 @@ struct Node* insertEnd(struct Node* head, int data) {
     }
     newNode->data = data;
 
-    // If the list is empty, make the new node point to itself
     if (head == NULL) {
         newNode->next = newNode;
         return newNode;
     }
 
-    // Otherwise, traverse to the last node
     struct Node* temp = head;
     while (temp->next != head) {
         temp = temp->next;
     }
 
-    // Update links to insert the new node at the end
     temp->next = newNode;
     newNode->next = head;
 
@@ -57,17 +50,13 @@ struct Node* insertEnd(struct Node* head, int data) {
 
 int main() {
     struct Node* head = NULL;
-
-    // Insert elements into the circular linked list
     head = insertEnd(head, 10);
     head = insertEnd(head, 20);
     head = insertEnd(head, 30);
     head = insertEnd(head, 40);
 
-    // Print the list
     printList(head);
 
-    // Free allocated memory (Good practice)
     if (head != NULL) {
         struct Node* current = head;
         struct Node* nextNode;
